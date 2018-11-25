8752af88b3fecdbaaed5894b2523f9d8baa8a1bf
anilamec <anilameec@gmail.com>
2018-11-25 17:59:54 -0600

    mmm
    mmmmm

--- beginner/Animal.java
+++ beginner/Animal.java
@@ -1,12 +1,14 @@
 package beginner;
+
 public class Animal {
-	
-	int a=10;
-	// This is eat method.
+
+	int a = 10;
+
+	// This is eat method.mmmmm
 	public void eat() {
 		System.out.println("All animal eat someting to survive.");
 	}
-	
+
 	public void walk() {
 		System.out.println("All animal walk but in a different way.");
 	}
