c0fa48014ff736b6558ca9120410b9bc51b80b6f
anilamec <anilameec@gmail.com>
2018-11-25 18:01:54 -0600

    whhhhhhh
    hhhhhhhh

--- beginner/CopyFile.java
+++ beginner/CopyFile.java
@@ -1,28 +1,32 @@
 package beginner;
-import java.io.*;
-import java.util.*;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CopyFile {
-	
-	public static void main (String args[]) throws IOException {
+
+	public static void main(String args[]) throws IOException {
 		File file = new File("C:\\Users\\Jiwan\\eclipse-workspace\\Beginers\\input.txt");
-		FileReader in =null;
-		FileWriter out =null;
+		FileReader in = null;
+		FileWriter out = null;
 		// This is test.
 		try {
-			in =new FileReader("input.txt");
+			in = new FileReader("input.txt");
 			out = new FileWriter("output.txt");
 			int c;
-			while ((c=in.read()) !=-1) {
+			while ((c = in.read()) != -1) {
 				out.write(c);
 			}
 		} finally {
-			if (in !=null) {
+			if (in != null) {
 				in.close();
 			}
-			if(out!=null) {
+			if (out != null) {
 				out.close();
+				// this is good
 			}
 		}
 	}
 }
-
